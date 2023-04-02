d3983df359797257aebac20a637a7478a4f8b573
vrk2299 <vivekreddykompelly99@gmail.com>
2023-04-03 03:56:17 +0530

    first commit lets see

---
+++ py.py
@@ -0,0 +1,2 @@
+iook here is my profile I am about to build and i am currently working on some things as well you will give me the possible jobs and freelance rate and freelance jobs I can apply to                                                      
+so it goes like

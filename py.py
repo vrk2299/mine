5e32dd810fa1efd20e86bf4b1d9074be18133078
vrk2299 <vivekreddykompelly99@gmail.com>
2023-04-03 04:01:24 +0530

    added some python into it first line

--- py.py
+++ py.py
@@ -1,2 +1,7 @@
-iook here is my profile I am about to build and i am currently working on some things as well you will give me the possible jobs and freelance rate and freelance jobs I can apply to                                                      
-so it goes like
+#iook here is my profile I am about to build and i am currently working on some things as well you will give me the possible jobs and freelance rate and freelance jobs I can apply to                                                      
+#so it goes like
+#fuck all this we are gonna write some python program which may or may not wor
+
+x = input('what ever the fuck this is')
+
+
